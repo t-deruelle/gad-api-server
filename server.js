22cb95fa4375ee26816a0a96b2be08c556735073
t-deruelle <tderuelle@e360s.ca>
2025-02-17 13:02:35 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use(cors());
