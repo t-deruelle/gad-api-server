946b3f82ce945210ad7a9438bc6c076aeedc3d5b
t-deruelle <tderuelle@e360s.ca>
2025-02-17 13:03:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`✅ Server running at http://0.0.0.0:${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
